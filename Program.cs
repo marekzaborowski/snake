a3068d1d79fe283b81d323578cc38a56615c93b0
Marek Zaborowski <marekzaborowski98@gmail.com>
2018-07-08 12:11:45 +0200

    waz wyswietla sie w czterech kierunkach

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 10;
+            int x = 10;
+            int k = 4;
+
+            waz w = new waz();
+            w.MetodaPozycyjna(y, x, k);
+            
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ waz.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class waz
+    {
+        public void MetodaPozycyjna(int wysokosc, int szerokosc, int kierunek)
+        {
+            if(kierunek == 4)
+            {
+                for (int i = 0; i < wysokosc - 3; i++)
+                {
+                    Console.WriteLine();
+                }
+                //Console.WriteLine("^");
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int i = 0; i < szerokosc; i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine("^");
+                }
+            }
+            else
+            for (int i = 0; i < wysokosc; i++)
+            {
+                Console.WriteLine();
+            }
+            if (kierunek == 3)
+            {
+                for (int j = 0; j < szerokosc - 3; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("<");
+                Console.Write("<");
+                Console.Write("<");
+            }
+            else
+            for (int j = 0; j < szerokosc; j++)
+            {   
+                Console.Write(" ");
+            }
+            if (kierunek == 1)
+            {
+                for (int q = 0; q < 3; q++)
+                {
+                    Console.WriteLine("v");
+                    for (int j = 0; j < szerokosc; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+            }
+            if(kierunek == 2)
+            {
+                Console.Write(">");
+                Console.Write(">");
+                Console.Write(">");
+            }
+             
+        }
+
+      
+    }
+}
