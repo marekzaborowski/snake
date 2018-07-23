cc8e8bca6be5f2d86f08add084a8564b46a6c629
Marek Zaborowski <marekzaborowski98@gmail.com>
2018-07-23 17:36:03 +0200

    modernizacja kodu

--- Program.cs
+++ Program.cs
@@ -9,12 +9,9 @@ namespace Snake
         {
             sort s = new sort();
             waz w = new waz();
-
-
-            int ostatniIndeks = 4;
+            int ostatniIndeks = 5;
             punkt[] pozycjaKlocka = new punkt[ostatniIndeks];
             punkt[] pozycjaKlocka2 = new punkt[ostatniIndeks];
-
 
             while (true)
             {
@@ -27,8 +24,6 @@ namespace Snake
                 int i = 0;
                 int j = 0;
 
-
-
                 for (int k=0; 1 < 2;)
                     {
                     if (Console.KeyAvailable)
@@ -37,71 +32,68 @@ namespace Snake
                     }
                     if (znak == x)
                     {
-                        punkt punkt11 = new punkt(i, j);
-                        punkt punkt22 = new punkt(i + 1, j);
-                        punkt punkt33 = new punkt(i + 2, j);
-                        punkt punkt44 = new punkt(i + 3, j);
-
-                        pozycjaKlocka[0] = punkt11;
-                        pozycjaKlocka[1] = punkt22;
-                        pozycjaKlocka[2] = punkt33;
-                        pozycjaKlocka[3] = punkt44;
+                        for(int q=0; q<ostatniIndeks; q++)
+                        {
+                            pozycjaKlocka[i] = new punkt(i, j);
+                            i++;
+                        }
+                        i = 0;
                     }
                     if (znak == g)
-                    {
-                        punkt punkt1 = new punkt(i, j);
-                        punkt punkt2 = new punkt(i + 1, j);
-                        punkt punkt3 = new punkt(i + 2, j);
-                        punkt punkt4 = new punkt(pozycjaKlocka[3].x+1, pozycjaKlocka[3].y);
-                       // i++;
-                        pozycjaKlocka[0] = pozycjaKlocka[1];
-                        pozycjaKlocka[1] = pozycjaKlocka[2];
-                        pozycjaKlocka[2] = pozycjaKlocka[3];
-                        pozycjaKlocka[3] = punkt4;
+                    {                      
+                        punkt zmienny = new punkt(pozycjaKlocka[ostatniIndeks-1].x+1, pozycjaKlocka[ostatniIndeks-1].y);
+                        for(int q=0; q<ostatniIndeks; q++)
+                        {
+                            if(q==ostatniIndeks-1)
+                            {
+                                pozycjaKlocka[q] = zmienny;
+                            }
+                            else
+                            pozycjaKlocka[q] = pozycjaKlocka[q+1];
+                        }
                     }
                     if (znak == f)
                     {                     
-                        punkt punkt1 = new punkt(i, j);
-                        punkt punkt2 = new punkt(i, j+1);
-                        punkt punkt3 = new punkt(i, j+2);
-                        punkt punkt4 = new punkt(pozycjaKlocka[3].x, pozycjaKlocka[3].y+1);
-                        // j++;
-                        pozycjaKlocka[0] = pozycjaKlocka[1];
-                        pozycjaKlocka[1] = pozycjaKlocka[2];
-                        pozycjaKlocka[2] = pozycjaKlocka[3];
-                        pozycjaKlocka[3] = punkt4;
+                        punkt zmienny = new punkt(pozycjaKlocka[ostatniIndeks-1].x, pozycjaKlocka[ostatniIndeks-1].y+1);
+                        for (int q = 0; q < ostatniIndeks; q++)
+                        {
+                            if (q == ostatniIndeks - 1)
+                            {
+                                pozycjaKlocka[q] = zmienny;
+                            }
+                            else
+                                pozycjaKlocka[q] = pozycjaKlocka[q + 1];
+                        }
                     }
                     if (znak == r)
                     {
-
-                        punkt punkt1 = new punkt(i, j);
-                        punkt punkt2 = new punkt(i, j + 1);
-                        punkt punkt3 = new punkt(i, j+2);
-                        punkt punkt4 = new punkt(pozycjaKlocka[3].x, pozycjaKlocka[3].y - 1);
-                        //j--;
-                        pozycjaKlocka[0] = pozycjaKlocka[1];
-                        pozycjaKlocka[1] = pozycjaKlocka[2];
-                        pozycjaKlocka[2] = pozycjaKlocka[3];
-                        pozycjaKlocka[3] = punkt4;
+                        punkt zmienny = new punkt(pozycjaKlocka[ostatniIndeks-1].x, pozycjaKlocka[ostatniIndeks-1].y - 1);
+                        for (int q = 0; q < ostatniIndeks; q++)
+                        {
+                            if (q == ostatniIndeks - 1)
+                            {
+                                pozycjaKlocka[q] = zmienny;
+                            }
+                            else
+                                pozycjaKlocka[q] = pozycjaKlocka[q + 1];
+                        }
                     }
                     if (znak == d)
                     {
-
-                        punkt punkt1 = new punkt(i, j);
-                        punkt punkt2 = new punkt(i + 1, j);
-                        punkt punkt3 = new punkt(i + 2, j);
-                        punkt punkt4 = new punkt(pozycjaKlocka[3].x-1, pozycjaKlocka[3].y);
-                        //  i--;
-                        pozycjaKlocka[0] = pozycjaKlocka[1];
-                        pozycjaKlocka[1] = pozycjaKlocka[2];
-                        pozycjaKlocka[2] = pozycjaKlocka[3];
-                        pozycjaKlocka[3] = punkt4;
+                        punkt zmienny = new punkt(pozycjaKlocka[ostatniIndeks-1].x-1, pozycjaKlocka[ostatniIndeks-1].y);
+                        for (int q = 0; q < ostatniIndeks; q++)
+                        {
+                            if (q == ostatniIndeks - 1)
+                            {
+                                pozycjaKlocka[q] = zmienny;
+                            }
+                            else
+                                pozycjaKlocka[q] = pozycjaKlocka[q + 1];
+                        }
                     }
                     Array.Copy(pozycjaKlocka, pozycjaKlocka2, ostatniIndeks);
-
                     s.Metodasortujacatablice(pozycjaKlocka2, ostatniIndeks - 1);
                     w.MetodaNowegoSpojrzeniaNaKonsole(pozycjaKlocka2, ostatniIndeks - 1);
-
                     Thread.Sleep(100);
                     Console.Clear();
                     }
